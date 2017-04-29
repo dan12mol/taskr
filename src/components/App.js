e2d4c3561c9b902034bdda5cd3a86a205c9383ae
Dan Moldovan <danmol@Dans-MacBook-Pro.local>
2017-04-29 13:37:51 +0300

    Moved authentication and logic to root component

--- src/components/App.js
+++ src/components/App.js
@@ -1,131 +1,43 @@
 import React from 'react';
-import base from '../base';
 
-import Login from './Login';
 import Dashboard from './Dashboard';
 
 class App extends React.Component {
-  
+
   constructor() {
     super();
 
-    this.authenticate = this.authenticate.bind(this);
-    this.authHandler = this.authHandler.bind(this);
-    this.logOut = this.logOut.bind(this);
-    this.addNewProject = this.addNewProject.bind(this);
-    this.showAddNewProject = this.showAddNewProject.bind(this);
-    this.closeAddNewProjectModal = this.closeAddNewProjectModal.bind(this);
-    this.removeProject = this.removeProject.bind(this);
     this.goToProject = this.goToProject.bind(this);
-
-    this.state = {
-      uid: null,
-      projects: {},
-      showAddNewProject: false
-    };
   }
 
-  componentDidMount() {
-    base.onAuth((user) => {
-      if (user) {
-        this.authHandler(null, {user});
-      }
-    });
-  }
-
-  componentWillUnmount() {
-    base.removeBinding(this.ref);
-  }
-
-  authenticate(service) {
-    base.authWithOAuthPopup(service, this.authHandler);
-  }
-
-  authHandler(err, authData) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    this.ref = base.syncState(`users/${authData.user.uid}/projects`, {
-      context: this,
-      state: 'projects'
-    });
-
-    this.setState({
-      uid: authData.user.uid,
-    });
-
-  }
-
-  logOut() {
-    base.unauth();
-    this.setState({
-      uid: null
-    });
-  }
-
-  addNewProject(project) {
-    const projects = {...this.state.projects};
-
-    const timestamp = Date.now();
-    projects[`project-${timestamp}`] = project;
-    
-    this.setState({ projects });
-  }
-  
-  removeProject(key) {
-
-    // TODO: Add a warning here before actually removing the project
-    const projects = {...this.state.projects};
-
-    projects[key] = null;
-    
-    this.setState({ projects });
-  }
-
-  showAddNewProject() {
-    this.setState({
-      showAddNewProject: true
-    });
-  }
-
-  closeAddNewProjectModal() {
-    this.setState({
-      showAddNewProject: false
-    });
-  }
-  
   goToProject(key) {
-    console.log(`going to: /project/${key}`);
     this.context.router.transitionTo(`/project/${key}`);
   }
-
+  
   render() {
-
-    if (!this.state.uid) {
-      return (
-        <div>
-          <Login authenticate={this.authenticate} />
-        </div>
-      )
-    }
-
     return (
       <div className="App">
-        <button className="add-new-project-btn b-w-btn" onClick={this.showAddNewProject}>+ Add New Project</button>
+        <button className="add-new-project-btn b-w-btn" onClick={this.props.showAddNewProject}>+ Add New Project</button>
         <Dashboard
-        addNewProject={this.addNewProject}
-        showAddNewProject={this.state.showAddNewProject}
-        closeAddNewProjectModal={this.closeAddNewProjectModal}
-        projects={this.state.projects}
-        removeProject={this.removeProject}
+        addNewProject={this.props.addNewProject}
+        showAddNewProject={this.props.showAddNewProject}
+        closeAddNewProjectModal={this.props.closeAddNewProjectModal}
+        projects={this.props.projects}
+        removeProject={this.props.removeProject}
         goToProject={this.goToProject}
         />
       </div>
     )
   }
 
+}
+
+App.propTypes = {
+  showAddNewProject: React.PropTypes.bool.isRequired,
+  addNewProject: React.PropTypes.func.isRequired,
+  closeAddNewProjectModal: React.PropTypes.func.isRequired,
+  projects: React.PropTypes.object.isRequired,
+  removeProject: React.PropTypes.func.isRequired,
 }
 
 App.contextTypes = {
--- src/index.js
+++ src/index.js
@@ -1,25 +1,132 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Match, Miss } from 'react-router';
+import base from './base';
 
 import './css/style.css';
 
 import NotFound from './components/NotFound';
 import App from './components/App';
 import TodoList from './components/TodoList';
+import Login from './components/Login';
 
-const Root = () => {
-  return (
-    <BrowserRouter>
-      <div>
-        <Match exactly pattern="/" component={App} />
-        <Match pattern="/project/:projectId" component={TodoList} />
-        <Miss component={NotFound} />
-      </div>
-    </BrowserRouter>
-  )
+class Root extends React.Component {
+
+  constructor() {
+    super();
+
+    this.authenticate = this.authenticate.bind(this);
+    this.authHandler = this.authHandler.bind(this);
+    this.logOut = this.logOut.bind(this);
+    this.addNewProject = this.addNewProject.bind(this);
+    this.showAddNewProject = this.showAddNewProject.bind(this);
+    this.closeAddNewProjectModal = this.closeAddNewProjectModal.bind(this);
+    this.removeProject = this.removeProject.bind(this);
+
+    this.state = {
+      uid: null,
+      projects: {},
+      showAddNewProject: false
+    };
+  }
+
+  componentDidMount() {
+    base.onAuth((user) => {
+      if (user) {
+        this.authHandler(null, {user});
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
+  authenticate(service) {
+    base.authWithOAuthPopup(service, this.authHandler);
+  }
+
+  authHandler(err, authData) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    this.ref = base.syncState(`users/${authData.user.uid}/projects`, {
+      context: this,
+      state: 'projects'
+    });
+
+    this.setState({
+      uid: authData.user.uid,
+    });
+
+  }
+
+  logOut() {
+    base.unauth();
+    this.setState({
+      uid: null
+    });
+  }
+
+  addNewProject(project) {
+    const projects = {...this.state.projects};
+
+    const timestamp = Date.now();
+    projects[`project-${timestamp}`] = project;
+    
+    this.setState({ projects });
+  }
+  
+  removeProject(key) {
+
+    // TODO: Add a warning here before actually removing the project
+    const projects = {...this.state.projects};
+
+    projects[key] = null;
+    
+    this.setState({ projects });
+  }
+
+  showAddNewProject() {
+    this.setState({
+      showAddNewProject: true
+    });
+  }
+
+  closeAddNewProjectModal() {
+    this.setState({
+      showAddNewProject: false
+    });
+  }
+  
+  render() {
+
+    if (!this.state.uid) {
+      return (
+        <div>
+          <Login authenticate={this.authenticate} />
+        </div>
+      )
+    }
+
+    return (
+      <BrowserRouter>
+        <div>
+          <Match exactly pattern="/" render={() => <App
+            showAddNewProject={this.state.showAddNewProject}
+            addNewProject={this.addNewProject}
+            closeAddNewProjectModal={this.closeAddNewProjectModal}
+            projects={this.state.projects}
+            removeProject={this.removeProject}
+            />} />
+          <Match pattern="/project/:projectId" render={() => <TodoList />}/>
+          <Miss component={NotFound} />
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
 
 render(<Root/>, document.querySelector('#root'));
-
-
